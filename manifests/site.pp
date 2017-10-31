a248730af68e1d4e089a1c511b82f17a9479bdcd
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-10-31 14:34:30 -0400

    f5 v5

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,49 @@ File { backup => false }
 site {
 
 }
+node 'bigip'  {
+f5_node { '/Common/WWW_Server_1':
+    ensure                   => 'present',
+    address                  => '52.207.82.45',
+    description              => 'WWW Server 1',
+    availability_requirement => 'all',
+    health_monitors          => ['/Common/icmp'],
+  }
+f5_node { '/Common/WWW_Server_2':
+    ensure                   => 'present',
+    address                  => '52.55.154.166',
+    description              => 'WWW Server 2',
+    availability_requirement => 'all',
+    health_monitors          => ['/Common/icmp'],
+  }
+f5_pool { '/Common/puppet_pool':
+    ensure                    => 'present',
+    members                   => [
+      { name => '/Common/WWW_Server_1', port => '80', },
+      { name => '/Common/WWW_Server_2', port => '80', },
+    ],
+    availability_requirement  => 'all',
+    health_monitors           => ['/Common/http_head_f5'],
+    require                   => [
+      F5_node['/Common/WWW_Server_1'],
+      F5_node['/Common/WWW_Server_2'],
+    ],
+  }
+  #f5_vlan { '/Common/Client': }
+f5_virtualserver { '/Common/puppet_vs':
+    ensure                    => 'present',
+    provider                  => 'standard',
+    default_pool              => '/Common/puppet_pool',
+    destination_address       => '54.156.241.113',
+    destination_mask          => '255.255.255.255',
+    http_profile              => '/Common/http',
+    service_port              => '80',
+    protocol                  => 'tcp',
+    source                    => '0.0.0.0/0',
+    #vlan_and_tunnel_traffic   => {'enabled' => ['/Common/Client']},
+    require                   => F5_pool['/Common/puppet_pool'],
+  }
+
 
 node 'master.inf.puppet.vm' {
   class {'f5': }

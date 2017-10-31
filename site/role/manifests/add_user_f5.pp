bf5f1de2704fa2f3c4d77b5a384eb5891cdc37e8
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-10-31 18:06:24 -0400

    F5 new v7

--- site/role/manifests/add_user_f5.pp
+++ site/role/manifests/add_user_f5.pp
@@ -1,17 +1,17 @@
 class role::add_user_f5 {
 
-f5_user { '/App1/greg1':
+f5_user { '/Users/greg1':
     name     => 'greg1',
     ensure   => 'present',
     password => 'greg1',
   }
-f5_user { '/App2/greg2':
+f5_user { '/Users/greg2':
     name     => 'greg2',
     ensure   => 'present',
     password => 'greg2',
   }
-f5_user { '/App1/greg3':
-    name     => '/App3/greg3',
+f5_user { '/Users/greg3':
+    name     => 'greg3',
     ensure   => 'present',
     password => 'greg3',
   }

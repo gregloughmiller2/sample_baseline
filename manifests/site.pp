00427eefcdd508024b7008d5847c0fec4e63b27d
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-10-31 14:44:54 -0400

    v6 f5

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,22 +7,31 @@ File { backup => false }
 site {
 
 }
-node 'bigip'  {
-f5_node { '/Common/WWW_Server_1':
+
+node default {
+  # Check if we've set the role for this node via trusted fact, pp_role.  If yes; include that role directly here.
+  if !empty( $trusted['extensions']['pp_role'] ) {
+    $role = $trusted['extensions']['pp_role']
+    include "role::${trusted['extensions']['pp_role']}"
+  }
+}
+
+node 'bigip' {
+  f5_node { '/Common/WWW_Server_1':
     ensure                   => 'present',
-    address                  => '52.207.82.45',
+    address                  => '52.207.82.245',
     description              => 'WWW Server 1',
     availability_requirement => 'all',
     health_monitors          => ['/Common/icmp'],
   }
-f5_node { '/Common/WWW_Server_2':
+  f5_node { '/Common/WWW_Server_2':
     ensure                   => 'present',
     address                  => '52.55.154.166',
     description              => 'WWW Server 2',
     availability_requirement => 'all',
     health_monitors          => ['/Common/icmp'],
   }
-f5_pool { '/Common/puppet_pool':
+  f5_pool { '/Common/puppet_pool':
     ensure                    => 'present',
     members                   => [
       { name => '/Common/WWW_Server_1', port => '80', },
@@ -36,7 +45,7 @@ f5_pool { '/Common/puppet_pool':
     ],
   }
   #f5_vlan { '/Common/Client': }
-f5_virtualserver { '/Common/puppet_vs':
+  f5_virtualserver { '/Common/puppet_vs':
     ensure                    => 'present',
     provider                  => 'standard',
     default_pool              => '/Common/puppet_pool',
@@ -49,16 +58,7 @@ f5_virtualserver { '/Common/puppet_vs':
     #vlan_and_tunnel_traffic   => {'enabled' => ['/Common/Client']},
     require                   => F5_pool['/Common/puppet_pool'],
   }
-
-
+}
 node 'master.inf.puppet.vm' {
   class {'f5': }
-}
-
-node default {
-  # Check if we've set the role for this node via trusted fact, pp_role.  If yes; include that role directly here.
-  if !empty( $trusted['extensions']['pp_role'] ) {
-    $role = $trusted['extensions']['pp_role']
-    include "role::${trusted['extensions']['pp_role']}"
-  }
 }

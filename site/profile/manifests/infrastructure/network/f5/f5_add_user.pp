bb32c721277d742ba48036e95acf3071d41da825
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-01 08:27:44 -0400

    f5 new syntax

--- site/profile/manifests/infrastructure/network/f5/f5_add_user.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_add_user.pp
@@ -1,22 +1,22 @@
 class profile::infrastructure::network::f5::f5_add_user {
 
 f5_user { '/Users/greg1':
-    name     => 'greg1',
+    name     => '/Users/greg1',
     ensure   => 'present',
     password => 'greg1',
   }
 f5_user { '/Users/greg2':
-    name     => 'greg2',
+    name     => '/Users/greg2',
     ensure   => 'present',
     password => 'greg2',
   }
 f5_user { '/Users/greg3':
-    name     => 'greg3',
+    name     => '/Users/greg3',
     ensure   => 'present',
     password => 'greg3',
   }
 f5_user { '/Users/greg4':
-    name     => '/User/greg4',
+    name     => '/Users/greg4',
     ensure   => 'present',
     password => 'greg4',
   }

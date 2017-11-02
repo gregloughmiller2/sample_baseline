d038849bb8b937147978597f5845d7027aecb895
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-01 23:47:27 -0400

    f5 command

--- site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
@@ -2,9 +2,7 @@ class profile::infrastructure::network::f5::f5_run_command {
 
 f5_command { '/Common/tmsh':
   tmsh => {
-    command => "delete auth",
-    name     => "Partition",
-    target   => "App2",
+    command => "delete auth Partition App2",
   }
   }
 }

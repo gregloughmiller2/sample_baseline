ce026fca5621e824f9d38132a394771bd4cdf4fc
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-01 21:03:15 -0400

    tttt

--- site/profile/manifests/infrastructure/network/f5/f5_delete_virtualserver.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_delete_virtualserver.pp
@@ -3,6 +3,13 @@ class profile::infrastructure::network::f5::f5_delete_virtualserver {
 f5_virtualserver { '/Common/puppet_vs1':
     ensure                    => 'absent',
     provider                  => 'standard',
+    default_pool              => '/Common/puppet_pool',
+    destination_address       => '10.0.0.200',
+    destination_mask          => '255.255.255.255',
+    http_profile              => '/Common/http',
+    service_port              => '80',
+    protocol                  => 'tcp',
+    source                    => '0.0.0.0/0',
   }
 f5_pool { '/Common/puppet_pool':
     ensure                    => 'absent',

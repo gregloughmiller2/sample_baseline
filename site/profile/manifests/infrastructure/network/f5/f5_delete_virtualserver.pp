8de894d4ccbd491ae23c7cfcdd8389da560b2b8d
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-01 20:53:57 -0400

    more

--- site/profile/manifests/infrastructure/network/f5/f5_delete_virtualserver.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_delete_virtualserver.pp
@@ -1,15 +1,19 @@
 class profile::infrastructure::network::f5::f5_delete_virtualserver {
 
-f5_node { '/Common/WWW_Server_1':
-    ensure                   => 'absent',
-  }
-f5_node { '/Common/WWW_Server_2':
-    ensure                   => 'absent',
-  }
-f5_pool { '/Common/puppet_pool':
-    ensure                    => 'absent',
-  }
 f5_virtualserver { '/Common/puppet_vs1':
     ensure                    => 'absent',
   }
+f5_pool { '/Common/puppet_pool':
+    ensure                    => 'absent',
+    require  =>  f5_virtualserver['/Common/puppet_vs1'],
+  }
+f5_node { '/Common/WWW_Server_1':
+    ensure                   => 'absent',
+    require  =>  f5_pool['/Common/puppet_pool'],
+  }
+f5_node { '/Common/WWW_Server_2':
+    ensure                   => 'absent',
+    require  =>  f5_node['/Common/WWW_Server_1'],
+  }
 }
+

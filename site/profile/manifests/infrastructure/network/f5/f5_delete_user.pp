de1002ac740c1a7597a2fa1c6ade536e7ea25ee2
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-01 21:25:54 -0400

    ggggg

--- site/profile/manifests/infrastructure/network/f5/f5_delete_user.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_delete_user.pp
@@ -1,23 +1,15 @@
 class profile::infrastructure::network::f5::f5_delete_user {
 
-f5_user { '/Users/greg1':
-    name     => '/Users/greg1',
+f5_user { 'greg1':
     ensure   => 'absent',
-    password => 'greg1',
   }
-f5_user { '/Users/greg2':
-    name     => '/Users/greg2',
+f5_user { 'greg2':
     ensure   => 'absent',
-    password => 'greg2',
   }
-f5_user { '/Users/greg3':
-    name     => '/Users/greg3',
+f5_user { 'greg3':
     ensure   => 'absent',
-    password => 'greg3',
   }
-f5_user { '/Users/greg4':
-    name     => '/Users/greg4',
+f5_user { 'greg4':
     ensure   => 'absent',
-    password => 'greg4',
   }
 }

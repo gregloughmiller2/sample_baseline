86a8b59b1869eb64c03a1b244ba16d3e497c111c
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-03 12:15:21 -0400

    tttt

--- site/profile/manifests/app/sample_website/delete_all_website.pp
+++ site/profile/manifests/app/sample_website/delete_all_website.pp
@@ -1,15 +1,12 @@
-class profile::app::sample_website::delete_all_website  (
-  String $doc_root = '/var/www/html',
-  Integer $webserver_port = 80,
-  String $website_source_dir = 'puppet:///modules/profile/app/sample_website'
-) {
+class profile::app::sample_website::delete_all_website  
+ {
 
   # configure apache
   apache::vhost { $::fqdn:
     ensure    => absent,
   }
 
-  file { "${doc_root}/index.html":
+  file { "/var/www/html/index.html":
     ensure  => absent,
   }
 

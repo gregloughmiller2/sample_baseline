3b81dc35e2449fb3e38c92965224e451f6655f95
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-08 14:58:51 -0500

    f5 comand

--- site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
@@ -2,7 +2,7 @@ class profile::infrastructure::network::f5::f5_run_command {
 
 f5_command { '/Common/tmsh':
   tmsh => {
-    ensure => "present"
+    ensure => "absent"
     command => "delete",
     name => "Partition",
     target => "App2",

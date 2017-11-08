3bf8e3acb8060b228e2784fe2a6ee7a0c5008e57
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-08 16:25:04 -0500

    f5 comand

--- site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
@@ -1,11 +1,12 @@
 class profile::infrastructure::network::f5::f5_run_command {
 
-f5_command { '/Common/tmsh':
+f5_command { '/Common/crap':
   tmsh => {
     ensure => "absent",
     command => "this is a message to f5",
     name => "car",
     target => "ford",
+    crap => "ford",
   }
   }
 }

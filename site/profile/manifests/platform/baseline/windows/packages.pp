d103270815cf8b8c0e29c250ec81ead419dd8156
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-11 10:09:42 -0500

    windows

--- site/profile/manifests/platform/baseline/windows/packages.pp
+++ site/profile/manifests/platform/baseline/windows/packages.pp
@@ -4,16 +4,16 @@ class profile::platform::baseline::windows::packages {
     ensure  => '7.4.2',
     provider => chocolatey,
   }
-  package { '7zip': 
-    ensure => latest,
-    provider => chocolatey,
-  }
-  package { 'git': 
-    ensure  => '2.14.1',
-    provider => chocolatey,
-  }
-  package { 'uniextract': 
-    ensure  => installed,
-    provider => chocolatey,
-
+#  package { '7zip': 
+#    ensure => latest,
+#    provider => chocolatey,
+#  }
+#  package { 'git': 
+#    ensure  => '2.14.1',
+#    provider => chocolatey,
+#  }
+#  package { 'uniextract': 
+#    ensure  => installed,
+#    provider => chocolatey,
+#  }
 }

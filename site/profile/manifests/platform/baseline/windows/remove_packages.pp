692c4c7fd496bd2210b74951ef6e04c955d7abbc
Greg Loughmiller <greg.loughmiller@greg.loughmiller-C02M54SNFD57>
2017-11-12 14:26:19 -0500

    win package cleanup

--- site/profile/manifests/platform/baseline/windows/remove_packages.pp
+++ site/profile/manifests/platform/baseline/windows/remove_packages.pp
@@ -2,14 +2,18 @@ class profile::platform::baseline::windows::remove_packages {
 
   package { 'notepadplusplus': 
     ensure  => absent,
+    provider  => chocolatey,
   }
   package { '7zip': 
     ensure => absent,
+    provider  => chocolatey,
   }
   package { 'git': 
     ensure => absent,
+    provider  => chocolatey,
   }
   package { 'putty': 
     ensure => absent,
+    provider  => chocolatey,
   }
 }

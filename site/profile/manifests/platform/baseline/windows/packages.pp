00689e546ba0c32397176a8ff29b1b8466479d20
puppet <puppet@localhost.local>
2017-11-16 12:05:19 +0000

    Update 'site/profile/manifests/platform/baseline/windows/packages.pp'

--- site/profile/manifests/platform/baseline/windows/packages.pp
+++ site/profile/manifests/platform/baseline/windows/packages.pp
@@ -1,15 +1,15 @@
 class profile::platform::baseline::windows::packages {
 
   package { 'notepadplusplus': 
-    ensure  => latest,
+    ensure  => '7.3.3',
     provider => chocolatey,
   }
   package { '7zip': 
-    ensure => latest,
+    ensure => '16.04',
     provider => chocolatey,
   }
   package { 'git': 
-    ensure  => latest,
+    ensure  => '2.14.2',
     provider => chocolatey,
   }
 }

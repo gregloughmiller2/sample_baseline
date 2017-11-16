948ca7f33ef53ceaf3a8446a9be9aa0182a6948d
puppet <puppet@localhost.local>
2017-11-16 12:28:38 +0000

    Update 'site/example/manifests/windows_regkeys.pp'

--- site/example/manifests/windows_regkeys.pp
+++ site/example/manifests/windows_regkeys.pp
@@ -15,5 +15,25 @@ class example::windows_regkeys {
     type => dword,
     data => '0xFFFFFFFF',
   }
+  
+  local_security_policy { 'Maximum password age':
+    ensure       => present,
+    policy_value => '40',
+  }
+
+  local_security_policy { 'Enforce password history':
+    ensure       => present,
+    policy_value => '60',
+  }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\fDisableCcm':
+    type => dword,
+    data => '1',
+  }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\fDisableCdm':
+    type => dword,
+    data => '1',
+  }
 
 }

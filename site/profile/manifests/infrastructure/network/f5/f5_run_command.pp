98181695570e2a003b2873e8787a4e034100ed01
puppet <puppet@localhost.local>
2017-12-05 22:09:48 +0000

    f5_command syntax

--- site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
@@ -1,6 +1,6 @@
 class profile::infrastructure::network::f5::f5_run_command {
 
 f5_command { '/Common/tmsh':
-  tmsh => "modify auth user greg1 password greg1 partition-access add { App1 {role manager }} shell tmsh"
+  tmsh => "modify auth user greg1 password greg1 partition-access add { App1 {role manager }} shell tmsh",
 
 }

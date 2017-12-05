5b0e55c3ef523e57a720604bc9f73a91e2b47711
puppet <puppet@localhost.local>
2017-12-05 22:28:17 +0000

    Update 'site/profile/manifests/infrastructure/network/f5/f5_run_command.pp'

--- site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
+++ site/profile/manifests/infrastructure/network/f5/f5_run_command.pp
@@ -1,6 +1,5 @@
 class profile::infrastructure::network::f5::f5_run_command {
 
 f5_command { '/Common/tmsh':
-  tmsh => "modify auth user greg1 password greg1 partition-access add { App1 {role manager }} shell tmsh",
-
+  tmsh => "list auth user greg1 ",
 }

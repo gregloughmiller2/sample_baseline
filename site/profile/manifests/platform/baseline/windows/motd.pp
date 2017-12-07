4511ef7357e6552314ae32311122b0cce4547bbe
puppet <puppet@localhost.local>
2017-12-07 14:06:07 +0000

    Update 'site/profile/manifests/platform/baseline/windows/motd.pp'

--- site/profile/manifests/platform/baseline/windows/motd.pp
+++ site/profile/manifests/platform/baseline/windows/motd.pp
@@ -1,20 +1,21 @@
 class profile::platform::baseline::windows::motd {
-  $motd = @("MOTD"/L)
-    ===========================================================
-
-          Welcome to ${::hostname}
-
-    Access  to  and  use of this server is  restricted to those
-    activities expressly permitted by the system administration
-    staff. If you are not sure if it is allowed, then DO NOT DO IT.
-
-    ===========================================================
-
-    The operating system is: ${::operatingsystem}
-            The domain is: ${::domain}
-
-    | MOTD
-
+#  $motd = @("MOTD"/L)
+#    ===========================================================
+#
+#          Welcome to ${::hostname}
+#
+#    Access  to  and  use of this server is  restricted to those
+#    activities expressly permitted by the system administration
+#    staff. If you are not sure if it is allowed, then DO NOT DO IT.#
+#
+#    ===========================================================#
+#
+#    The operating system is: ${::operatingsystem}
+#            The domain is: ${::domain}#
+#
+#    | MOTD
+#
+  $motd = "Welcome to ${::hostname}"
 
   # Check if we have a hiera override for the MOTD, otherwise use the default
   $message = lookup('motd', String, 'first', $motd)
